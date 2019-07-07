f048c10e75f9c770b03290b55c05d1c58e447360
Saketh Saraswathi <ssaketh97@gmail.com>
2019-07-07 21:56:00 +0530

    feat: Added basic numpy commands to the code
    Added the basic commands of a numpy library on 1d array and few easy 
    commands to know the array details.
    
    commands_type: basic

--- Numpy_commands.py
+++ Numpy_commands.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+#basic commands
+array_1d = np.array([10,22,53,14,75])     #to create a 1D array using numpy
+print(array_1d)
+
+len(array_1d)    #display length of array
+
+array_1d.sum()   #display sum of elements in array
+
+array_1d.max()  #display mamimum value element in array
+
+array_1d.min() #display minimum value element in array
