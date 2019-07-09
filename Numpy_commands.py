8c93c35fcac524193b1891500c98f05cbcc36298
Saketh Saraswathi <ssaketh97@gmail.com>
2019-07-09 23:34:46 +0530

    feat : Added random library commands to the code
    random library commands are most widely used in world in varoius cases
    
    command_type:random library

--- Numpy_commands.py
+++ Numpy_commands.py
@@ -11,3 +11,13 @@ array_1d.sum()   #display sum of elements in array
 array_1d.max()  #display mamimum value element in array
 
 array_1d.min() #display minimum value element in array
+
+#random library
+
+np.random.rand(3,2) #create a 3x2 array of random values between 0 and 1
+
+np.random.randn(3,3) #creates a trandom 3x3 array of values
+
+np.random.randint(5,size=10) #create array of size 10 with values between 0-5(exclusive)
+
+ np.random.random_sample() #returns a random sample between 0.0(exclusive) and 1.0
